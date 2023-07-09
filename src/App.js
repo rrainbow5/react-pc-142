827a9c76708aa1727199227e4988d87e3f50cef2
rrainbow5 <wangxingyuan233@gmail.com>
2023-07-09 23:28:39 +0800

    initial commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Layout from './pages/Layout';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      <Routes>
+        {/* 创建路由path与组件对应关系 */}
+        <Route path='/' element={<Layout />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
+      </div>
+
+    </BrowserRouter>
+
   );
 }
 
