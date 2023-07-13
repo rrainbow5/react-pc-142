a3ecb17152d02db5cc961a6f4ca50af17bc2bdcd
reins <wangxingyuan233@gmail.com>
2023-07-13 23:05:40 +0800

    更改配置文件；

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,32 @@
-import { Card } from "antd";
+import {Card, Form, Button, Checkbox, Input} from 'antd'
 import logo from '@/assets/logo.png'
-
 
 function Login() {
   return (
     <div className="login">
-      <Card className="login-container"/>
-      <img className="login-logo" src={logo} alt=""/>
+      <Card className="login-container" />
+      <img className="login-logo" src={logo} alt="" />
       {/* 登陆菜单 */}
-    </div>);
+      <Form>
+        <Form.Item>
+          <Input size="large" placeholder="请输入手机号" />
+        </Form.Item>
+        <Form.Item>
+          <Input size="large" placeholder="请输入验证码" />
+        </Form.Item>
+        <Form.Item>
+          <Checkbox className="login-checkbox-label">
+            我已阅读并同意「用户协议」和「隐私条款」
+          </Checkbox>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" size="large" block>
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
 }
 
-export default Login;
+export default Login
