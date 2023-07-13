18af78bfd73c33f08be72e0bd88336b070d07215
reins <wangxingyuan233@gmail.com>
2023-07-13 23:51:23 +0800

    搭建登录页面

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import './index.scss'
 
 // 引入index.scss
 import "./index.scss";
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,30 +1,30 @@
-import {Card, Form, Button, Checkbox, Input} from 'antd'
 import logo from '@/assets/logo.png'
+import {Card, Form, Input, Checkbox, Button} from 'antd'
 
 function Login() {
   return (
     <div className="login">
-      <Card className="login-container" />
-      <img className="login-logo" src={logo} alt="" />
-      {/* 登陆菜单 */}
-      <Form>
-        <Form.Item>
-          <Input size="large" placeholder="请输入手机号" />
-        </Form.Item>
-        <Form.Item>
-          <Input size="large" placeholder="请输入验证码" />
-        </Form.Item>
-        <Form.Item>
-          <Checkbox className="login-checkbox-label">
-            我已阅读并同意「用户协议」和「隐私条款」
-          </Checkbox>
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" size="large" block>
-            登录
-          </Button>
-        </Form.Item>
-      </Form>
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt=''/>
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Checkbox className="login-checkbox-label">
+              我已阅读并同意「用户协议」和「隐私条款」
+            </Checkbox>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   )
 }
